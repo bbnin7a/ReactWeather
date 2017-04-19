161ea219decb7926b28a5c7a1cf160e98bb06c81
bbnin7a <bbnin7a@gmail.com>
2017-04-19 18:55:15 +0800

    Prep ...

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http')) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
-});
-
-
+);
 app.use(express.static('public'));
 
-app.listen(port, function () {
+app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
 });
